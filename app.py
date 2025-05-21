208cef0910307376e191957a77c3c82086a6d692
Monish Nallagondalla <nsmonish@gmail.com>
2025-05-21 23:58:08 +0530

    page title and config

--- app.py
+++ app.py
@@ -8,3 +8,10 @@ from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from sqlalchemy import create_engine
 import sqlite3
 from langchain_groq import ChatGroq
+
+
+st.set_page_config(page_title="LangChain: Chat with SQL DB", page_icon="🦜")
+st.title("🦜 LangChain: Chat with SQL DB")
+
+LOCALDB="USE_LOCALDB"
+MYSQL="USE_MYSQL"
