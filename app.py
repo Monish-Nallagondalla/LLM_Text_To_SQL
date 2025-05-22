8f363ca802bc91a3fe496c476b63550d0b944076
Monish Nallagondalla <nsmonish@gmail.com>
2025-05-22 14:54:13 +0530

    app.py in progress

--- app.py
+++ app.py
@@ -15,3 +15,49 @@ st.title("🦜 LangChain: Chat with SQL DB")
 
 LOCALDB="USE_LOCALDB"
 MYSQL="USE_MYSQL"
+
+radio_opt = ["Use SQLLite 3 Database- Student.db","Connect to you MySQL Database"]
+
+selected_opt = st.sidebar.radio(label="choose the DB which you want to chat",options = radio_opt)
+
+if radio_opt.index(selected_opt==1): # the first one from the options of radio_opt
+
+    db_uri = MYSQL
+    mysql_host = st.sidebar.text_input("Provide MySql Host")
+    mysql_user = st.sidebar.text_input('MySQL user')
+    mysql_password = st.sidebar.text_input("MYSQL password",type='Password')
+    mysql_db=st.sidebar.text_input("MySQL database")
+
+else:
+    db_uri = LOCALDB
+
+api_key = st.sidebar.text_input(label="GRoq API Key",type="password")
+
+if not db_uri:
+    st.info("Please enter the database information and Uri")
+
+if not api_key:
+    st.info("Please add the groq api key")
+
+llm = ChatGroq(groq_api_key = api_key,model_name="Llama3-8b-8192",streaming=True)
+
+@st.cache_resource(ttl='2h')
+
+def configure_db (db_uri , mysql_host=None,mysql_user=None,mysql_password=None,mysql_db=None):
+    if db_uri==LOCALDB:
+        dbfilepath=(Path(__file__).parent/"student.db").absolute()
+        print(dbfilepath)
+        creator = lambda: sqlite3.connect(f"file:{dbfilepath}?mode=ro",uri=True)
+        return SQLDatabase(create_engine("sqlite:///",creator=creator))
+    elif db_uri==MYSQL:
+        if not (mysql_host and mysql_user and mysql_password and mysql_db):
+            st.error("Please pr")
+
+
+
+
+        db= configure_db(db_uri,mysql_host,mysql_user,mysql_password,mysql_db)
+    else:
+        db =configure_db(db_uri)
+
+
